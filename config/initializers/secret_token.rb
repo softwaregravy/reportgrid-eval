e505517f9684939e77fd33da2b4a4ff1f18ac432
John Hinnegan <john.hinnegan@gmail.com>
2012-01-08 14:37:48 -0800

    raw rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReportgridEval::Application.config.secret_token = 'd52e193a4a1783e926853a7a376e611efe64b674086ba0f19a25e9b7db52da47572d7cfc8c0f6d1659107d21ab26b8781bda6f581000ed95543ea881dc91489b'
